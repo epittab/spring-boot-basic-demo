fa65f6a10ad65912e0510b89a205a3a4a93c41c3
epittab <ep7m@me.com>
2020-11-28 12:15:04 -0500

    first commit

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,20 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+
+public class FakePersonDataAccessService implements PersonDAO {
+
+    private static List<Person> DB = new ArrayList<Person>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        // TODO Auto-generated method stub
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+    
+}
